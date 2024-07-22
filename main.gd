7d818a35674525487c97506ec8b1d27daf72fd01
Shuflduf <shuflduf@gmail.com>
2024-07-22 14:46:37 -0600

    left joystick

--- main.gd
+++ main.gd
@@ -2,6 +2,7 @@ extends Control
 
 @onready var pressed_buttons: Control = %FaceButtons
 @onready var right_stick: TextureRect = $Joysticks/Right
+@onready var left_stick: TextureRect = $Joysticks/Left
 
 @export var stick_mult = 10
 
@@ -11,6 +12,10 @@ func _input(event: InputEvent) -> void:
 	
 	if event is InputEventJoypadMotion:
 		match event.axis:
+			0:
+				left_stick.position.x = event.axis_value * stick_mult
+			1:
+				left_stick.position.y = event.axis_value * stick_mult
 			2:
 				right_stick.position.x = event.axis_value * stick_mult
 			3:
